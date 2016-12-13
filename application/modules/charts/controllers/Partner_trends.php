96be8720ab71346b147b351b29b7cce814e0cff1
bakasajoshua <baksajoshua09@gmail.com>
2016-12-13 18:14:12 +0300

    All the remaining changes to be reviewed

---
+++ application/modules/charts/controllers/Partner_trends.php
@@ -0,0 +1,67 @@
+<?php
+defined("BASEPATH") or exit("No direct script access allowed!");
+
+/**
+* 
+*/
+class Partner_trends extends MY_Controller
+{
+	
+	function __construct()
+	{
+		parent:: __construct();
+		$this->load->model('partner_trends_model');
+	}
+
+	function positive_trends($partner=NULL){
+		$obj = $this->partner_trends_model->yearly_trends($partner);
+		// echo "<pre>";print_r($obj);echo "</pre>";die();
+
+		$data['trends'] = $obj['suppression_trends'];
+		$data['title'] = "Suppression Trends";
+		$data['div_name'] = "suppression";
+		$data['suffix'] = "%";
+		$data['yAxis'] = "Suppression Rate (%)";
+		$this->load->view('yearly_trends_view', $data);
+
+		$data['trends'] = $obj['test_trends'];
+		$data['title'] = "Test Trends";
+		$data['div_name'] = "tests";
+		$data['suffix'] = "";
+		$data['yAxis'] = "Number of  Valid Tests";
+		$this->load->view('yearly_trends_view', $data);
+
+		
+
+		$data['trends'] = $obj['rejected_trends'];
+		$data['title'] = "Rejected Trends";
+		$data['div_name'] = "rejects";
+		$data['suffix'] = "%";
+		$data['yAxis'] = "Rejects (%)";
+		$this->load->view('yearly_trends_view', $data);
+
+
+		// $data['trends'] = $obj['tat4_trends'];
+		// $data['title'] = "Turnaround Time";
+		// $data['div_name'] = "tat";
+		// $data['suffix'] = "";
+		// $data['yAxis'] = "Tat4 Time";
+		// $this->load->view('yearly_trends_view', $data);
+
+		
+
+		//echo json_encode($obj);
+		//echo "<pr>";print_r($obj);die;
+
+	}
+
+	function summary($partner=NULL){
+		$data['trends'] = $this->partner_trends_model->yearly_summary($partner);
+		//$data['trends'] = $this->positivity_model->yearly_summary();
+		//echo json_encode($data);
+		// echo "<pre>";print_r($data);die();
+		$this->load->view('trends_outcomes_view', $data);
+	}
+
+
+}
