e2943db357845945288961d1e894489ea775147c
bakasajoshua <baksajoshua09@gmail.com>
2017-05-02 16:43:36 +0300

    Live page pure template

---
+++ application/modules/live/controllers/Live.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') or die('No direct script access allowed');
+/**
+* 
+*/
+class Live extends MY_Controller
+{
+	
+	function __construct()
+	{
+		parent:: __construct();
+		$this->data	=	array_merge($this->data,$this->load_libraries(array('material','highstock','highmaps','highcharts','custom','tablecloth','select2')));
+		$this->data['live'] = TRUE;
+	}
+
+	function index()
+	{
+		$this->data['content_view'] = 'live/live_view';
+		// echo "<pre>";print_r($this->data);die();
+		$this -> template($this->data);
+	}
+}
+?>
---
+++ application/modules/live/views/live_view.php
@@ -0,0 +1,173 @@
+<div class="row">
+	
+</div>
+<div class="row">
+	<div class="col-md-2">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				Basic panel example
+			</div>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				Basic panel example
+			</div>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				Basic panel example
+			</div>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				Basic panel example
+			</div>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				Basic panel example
+			</div>
+		</div>
+	</div>
+	<div class="col-md-2">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				Basic panel example
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Panel title</h3>
+			</div>
+			<div class="panel-body">
+				Panel content
+			</div>
+		</div>
+	</div>
+</div>
