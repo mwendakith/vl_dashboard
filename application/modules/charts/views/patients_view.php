6367552cc2db74fd4da3056ee22f75c4d517cdce
bakasajoshua <baksajoshua09@gmail.com>
2017-05-12 14:04:37 +0300

    css

--- application/modules/charts/views/patients_view.php
+++ application/modules/charts/views/patients_view.php
@@ -1,25 +1,24 @@
-<div style="width: 540px;">
-	<p>
-		Total Patients : <?php echo $patients; ?>  <br />
-		Total VL done : <?php echo $tests; ?>  <br />
-		Unmet Need : <?php echo $unmet; ?>%  <br />
+<p style="height: 200px;">
+	Total Patients : <?php echo $patients; ?>  <br />
+	Total VL done : <?php echo $tests; ?>  <br />
+	Unmet Need : <?php echo $unmet; ?>%  <br />
 
-	</p>
+</p>
 
-
-	<table id="example" cellspacing="1" cellpadding="3" class="tablehead table table-striped table-bordered" style="max-width: 100%;">
-		<thead>
-			<tr class="colhead">
-				<th>Total Tests</th>
-				<th>1 VL</th>
-				<th>2 VL</th>
-				<th>3 VL</th>
-				<th>> 3 VL</th>
-			</tr>
-			
-		</thead>
-		<tbody>
-			<?php echo $stats;?>
-		</tbody>
-	</table>
+<div style="height: 340px;">
+<table id="example" cellspacing="1" cellpadding="3" class="tablehead table table-striped table-bordered" style="max-width: 100%;">
+	<thead>
+		<tr class="colhead">
+			<th>Total Tests</th>
+			<th>1 VL</th>
+			<th>2 VL</th>
+			<th>3 VL</th>
+			<th>> 3 VL</th>
+		</tr>
+		
+	</thead>
+	<tbody>
+		<?php echo $stats;?>
+	</tbody>
+</table>
 </div>
