5a9729182fb4a295311245fead3b58828476fcac
bakasajoshua <baksajoshua09@gmail.com>
2017-10-05 13:27:52 +0300

    This for the age gender

--- dbv/data/schema/all_vl.sql
+++ dbv/data/schema/all_vl.sql
@@ -3479,18 +3479,21 @@ BEGIN
     
 END //
 DELIMITER ;
+
 DROP PROCEDURE IF EXISTS `proc_get_vl_age_gender`;
 DELIMITER //
 CREATE PROCEDURE `proc_get_vl_age_gender`
 (IN A_id INT(11), IN filter_year INT(11), IN from_month INT(11), IN to_year INT(11), IN to_month INT(11))
 BEGIN
   SET @QUERY =    "SELECT
-        SUM(`maletest`) AS `maletest`,
-        SUM(`femaletest`) AS `femaletest`,
-        SUM(`nogendertest`) AS `nodata`
+        SUM(`maletest`-`malenonsuppressed`) AS `malesuppressed`,
+        SUM(`malenonsuppressed`) AS `malenonsuppressed`, 
+        SUM(`femaletest`-`femalenonsuppressed`) AS `femalesuppressed`,
+        SUM(`femalenonsuppressed`) AS `femalenonsuppressed`, 
+        SUM(`nogendertest`-`nogendernonsuppressed`) AS `nodatasuppressed`,
+        SUM(`nogendernonsuppressed`) AS `nodatanonsuppressed`
     FROM `vl_national_age`
     WHERE 1 ";
-
   
     IF (from_month != 0 && from_month != '') THEN
       IF (to_month != 0 && to_month != '' && filter_year = to_year) THEN
@@ -3514,6 +3517,7 @@ BEGIN
      EXECUTE stmt;
 END //
 DELIMITER ;
+
 DROP PROCEDURE IF EXISTS `proc_get_vl_age_outcomes`;
 DELIMITER //
 CREATE PROCEDURE `proc_get_vl_age_outcomes`
