f8f9da76710fbdff4016d531f6f5d1dc8542d047
bakasajoshua <baksajoshua09@gmail.com>
2017-10-19 17:23:40 +0300

    Started working on the PMTCT

---
+++ application/modules/partner/views/partner_pmtct__footer_view.php
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+	$().ready(function(){
+		$.get("<?php echo base_url();?>template/dates", function(data){
+    		obj = $.parseJSON(data);
+	
+			if(obj['month'] == "null" || obj['month'] == null){
+				obj['month'] = "";
+			}
+			$(".display_date").html("( "+obj['year']+" "+obj['month']+" )");
+			$(".display_range").html("( "+obj['prev_year']+" - "+obj['year']+" )");
+    	});
+		$("#second").hide();
+		// fetching the partner outcomes
+		$("#partner_div").load("<?php echo base_url('charts/summaries/county_outcomes'); ?>/"+null+"/"+null+"/"+1);
+	});
+</script>
---
+++ application/modules/partner/views/partner_pmtct_view.php
@@ -0,0 +1,27 @@
+<style type="text/css">
+	.display_date {
+		width: 130px;
+		display: inline;
+	}
+	.display_range {
+		width: 130px;
+		display: inline;
+	}
+</style>
+
+<div class="row" id="first">
+	<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="panel panel-default">
+		  <div class="panel-heading">
+		  	Partner Outcomes <div class="display_date"></div>
+		  </div>
+		  <div class="panel-body" id="partner_div">
+		    <center><div class="loader"></div></center>
+		  </div>
+		</div>
+	</div>
+</div>
+<div class="row" id="second">
+	
+</div>
+<?= @$this->load->view('partner/partner_pmtct__footer_view');?>
