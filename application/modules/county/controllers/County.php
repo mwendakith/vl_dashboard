65147e8f27774775e3594c497a8afd339710c9a3
bakasajoshua <baksajoshua09@gmail.com>
2017-11-21 12:52:27 +0300

    County PMTCT complete.

--- application/modules/county/controllers/County.php
+++ application/modules/county/controllers/County.php
@@ -30,6 +30,16 @@ class County extends MY_Controller {
 		$this->template($this->data);
 	}
 
+	public function pmtct()
+	{
+		$this->load->module('charts/summaries');
+		$this->load->module('charts/pmtct');
+		$this->clear_all_session_data();
+		$this->data['content_view'] = 'county/county_pmtct_view';
+		// echo "<pre>";print_r($this->data);die();
+		$this -> template($this->data);
+	}
+
 	function countyMap()
 	{
 		$this->data['county'] = TRUE;
