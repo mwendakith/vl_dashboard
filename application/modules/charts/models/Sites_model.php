e27d15e8a9826a75c452c5775994b75b98dc8590
Unknown <baksajoshua09@gmail.com>
2018-12-11 12:38:19 +0300

    Remove the limits in an attempt to optimize the code

--- application/modules/charts/models/Sites_model.php
+++ application/modules/charts/models/Sites_model.php
@@ -153,8 +153,8 @@ class Sites_model extends MY_Model
 				}
 			}
 		}
-		ini_set("memory_limit", "-1");
-        ini_set("max_execution_time", "3000");
+		// ini_set("memory_limit", "-1");
+        // ini_set("max_execution_time", "3000");
 		foreach ($counties as $key => $value) {
 			foreach ($resultage as $k => $v) {
 				if ($value == $v->selection) {
