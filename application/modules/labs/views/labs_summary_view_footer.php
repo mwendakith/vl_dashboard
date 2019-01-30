d340eca585e60752bc31bfd5682da309ac75daaf
Joel <joelkith@gmail.com>
2019-01-30 14:37:01 +0300

    POC

--- application/modules/labs/views/labs_summary_view_footer.php
+++ application/modules/labs/views/labs_summary_view_footer.php
@@ -208,9 +208,7 @@
 		var to_year = localStorage.getItem("to_year");
 		var to_month = localStorage.getItem("to_month");
 
-		$("#empty_div").load("<?php echo base_url();?>charts/labs/results_outcome/"+year+"/"+month+"/"+to_year+"/"+to_month);
-
-
+		$("#empty_div").load("<?php echo base_url();?>charts/labs/results_outcome/"+facility_id+"/"+year+"/"+month+"/"+to_year+"/"+to_month);
 	}
 
 </script>
