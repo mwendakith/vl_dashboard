4ec48ecc91e1e547cb1bd4840a18477400e61978
Joel <joelkith@gmail.com>
2019-04-03 21:04:51 +0300

    Variable current suppression

--- application/modules/partner/views/partner_summary_view.php
+++ application/modules/partner/views/partner_summary_view.php
@@ -78,7 +78,6 @@
 			</div>
 		</div>
 
-	<div class="row">
 		<div class="col-md-4 col-sm-4 col-xs-12">
 			<div class="panel panel-default">
 			  <div class="panel-heading">
@@ -111,7 +110,7 @@
 			  </div>
 			</div>
 		</div>
-	</div>	
+
 </div>
 
 <div class="row" id="first">
