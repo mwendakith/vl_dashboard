2d8efcb763b262008c41cbce9afcee84a4e2fa69
bakasajoshua <baksajoshua09@gmail.com>
2019-04-24 15:19:54 +0300

    Use the site thing

--- application/modules/charts/models/Sites_model.php
+++ application/modules/charts/models/Sites_model.php
@@ -430,9 +430,9 @@ class Sites_model extends MY_Model
 		$result = $this->db->query($sql)->result_array();
 
 		$this->db->close();
-
-		$params = "patient/facility/{$facility}/{$type}/{$year}/{$month}/{$to_year}/{$to_month}";
-
+		$params = "patient/suppression/site/{$facility}/{$type}/{$year}/{$month}/{$to_year}/{$to_month}";
+		// $params = "patient/facility/{$facility}/{$type}/{$year}/{$month}/{$to_year}/{$to_month}";
+		
 		$res = $this->req($params);
 		
 		echo "<pre>";print_r($res);die();
@@ -1141,7 +1141,7 @@ class Sites_model extends MY_Model
 
 		$facility = $query->facilitycode;
 
-		$params = "patient/suppression/site/{$facility}/{$type}/{$year}/{$month}/{$to_year}/{$to_month}";
+		$params = "patient/facility/{$facility}/{$type}/{$year}/{$month}/{$to_year}/{$to_month}";
 
 		$result = $this->req($params);
 
