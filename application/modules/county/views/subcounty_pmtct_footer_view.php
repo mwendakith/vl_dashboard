5ecc9a833e5fd86427d1e05a2d6c20486932405b
bakasajoshua <baksajoshua09@gmail.com>
2019-07-16 10:32:59 +0300

    Updates till on change on the sub county drop down

--- application/modules/county/views/subcounty_pmtct_footer_view.php
+++ application/modules/county/views/subcounty_pmtct_footer_view.php
@@ -16,5 +16,45 @@
 		$("#fourth").hide();
 
 		$("#subcounty_div").load("<?= @base_url('charts/subcounties/subcounty_outcomes'); ?>");
+
+		$("#sub_county_drpd").change(function(){
+			subcounty = $(this).val();
+			var posting = $.post( "<?= @base_url();?>template/filter_sub_county_data", { subCounty: subcounty } );
+			// After setting the sub county data
+			posting.done(function( subcounty ) {
+				if (subcounty==null||subcounty=='null'||subcounty==undefined||subcounty=='') 
+	        		subcounty = null
+	        	$.get("<?php echo base_url();?>template/breadcrum/"+subcounty+"/"+null+"/"+null+"/"+1, function(data){
+	        		$("#breadcrum").html(data);
+	        	});
+	        	$.get("<?php echo base_url();?>template/dates", function(data){
+	        		obj = $.parseJSON(data);
+			
+					if(obj['month'] == "null" || obj['month'] == null){
+						obj['month'] = "";
+					}
+					$(".display_date").html("( "+obj['year']+" "+obj['month']+" )");
+					$(".display_range").html("( "+obj['prev_year']+" - "+obj['year']+" )");
+	        	});
+	        	if (em=="NA") {
+	        		$("#first").show();
+	        		$("#second").hide();
+					$("#third").hide();
+					$("#fourth").hide();
+	        	} else {
+	        		subcounty = JSON.parse(subcounty);
+	        		$("#first").hide();
+	        		$("#second").show();
+					$("#third").hide();
+					$("#fourth").hide();
+					// Initialize the loader
+					$("#pmtct_outcomes_div").html("<center><div class='loader'></div></center>");
+					$("#pmtct_sup_outcomes_div").html("<center><div class='loader'></div></center>");
+					// Display the loaded data
+					$("#pmtct_outcomes_div").load("<?= @base_url('charts/pmtct/pmtct_outcomes'); ?>/"+null+"/"+null+"/"+1+"/"+null+"/"+null+"/"+null+"/"+null+"/"+null+"/"+subcounty);
+					$("#pmtct_sup_outcomes_div").load("<?= @base_url('charts/pmtct/pmtct_outcomes'); ?>/"+null+"/"+null+"/"+2+"/"+null+"/"+null+"/"+null+"/"+null+"/"+null+"/"+subcounty);
+	        	} 
+			});
+		});
 	});
 </script>
