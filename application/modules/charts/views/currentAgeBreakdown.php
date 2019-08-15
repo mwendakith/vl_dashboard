3690e1ecf17ecf27a53e1222d98bf6ace6e1bf28
bakasajoshua <baksajoshua09@gmail.com>
2019-08-15 16:13:48 +0300

    New Summary page

--- application/modules/charts/views/currentAgeBreakdown.php
+++ application/modules/charts/views/currentAgeBreakdown.php
@@ -3,7 +3,7 @@
 		<h3>0-24</h3>
 	</div>
 	<div class="col-xs-4">
-		<h4>Total VLs: </h4> <?= @$less25; ?>
+		<h4>Total VLs: </h4> <?= @($less2+$less9+$less14+$less19+$less24); ?>
 	</div>
 	<div class="col-xs-4">
 		<h4>Male: </h4> 
@@ -17,7 +17,7 @@
 		<h3>&gt; 24</h3>
 	</div>
 	<div class="col-xs-4">
-		<h4>Total VLs: </h4> <?= @$above25; ?>
+		<h4>Total VLs: </h4> <?= @$over25; ?>
 	</div>
 	<div class="col-xs-4">
 		<h4>Male: </h4> 
