697ce4bc2af3207301915e903abb9a942c4edeaf
bakasajoshua <baksajoshua09@gmail.com>
2019-08-16 05:26:28 +0300

    New Summary page

--- application/modules/charts/controllers/Summaries.php
+++ application/modules/charts/controllers/Summaries.php
@@ -332,5 +332,12 @@ class Summaries extends MY_Controller {
     	$this->load->view('currentAgeBreakdown',$data);
 	}
 
+	function current_summary_suppression_age($type=null,$id=null,$first=TRUE) {
+		$data['outcomes'] = $this->currentSuppressionModel->current_age($type,$id);
+		echo "<pre>";print_r($first);die();
+		$data['div_name'] = 'current_age_pie';
+    	$this->load->view('justification_view',$data);
+	}
+
 }
 ?>
